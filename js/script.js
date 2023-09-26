d29b36fb302230493a9c08316a67e09075b400e1
BrunoDaPaz <nobrudapaz@gmail.com>
2023-09-26 00:41:10 -0300

    Ajustes

--- js/script.js
+++ js/script.js
@@ -1,26 +1,31 @@
-openMenu.addEventListener('click', () => {
-    menu.style.display = 'flex'
-    menu.style.right = (menu.offsetWidth * -1) + 'px'
-    openMenu.style.display = 'none'
-    setTimeout(() => {
-        menu.style.opacity = '1'
-        menu.style.right = '0'
-    }, 10)
-})
-
-closeMenu.addEventListener('click', () => {
-    menu.style.opacity = '0'
-    menu.style.right = (menu.offsetWidth * -1) + 'px'
-    setTimeout(() => {
-        menu.removeAttribute('style')
-        openMenu.removeAttribute('style')
-
-    }, 200)
-
-})
-
+const openMenu = document.getElementById('openMenu');
+const closeMenu = document.getElementById('closeMenu');
+const menu = document.getElementById('menu');
 const imagemClicavel = document.getElementById('logo');
 
-imagemClicavel.addEventListener('click', function() {
-    window.location.href = '#';
+openMenu.addEventListener('click', () => {
+    menu.style.display = 'flex';
+    menu.style.right = (menu.offsetWidth * -1) + 'px';
+    openMenu.style.display = 'none';
+    setTimeout(() => {
+        menu.style.opacity = '1';
+        menu.style.right = '0';
+    }, 10);
 });
+
+closeMenu.addEventListener('click', () => {
+    menu.style.opacity = '0';
+    menu.style.right = (menu.offsetWidth * -1) + 'px';
+    setTimeout(() => {
+        menu.removeAttribute('style');
+        openMenu.removeAttribute('style');
+    }, 200);
+});
+
+imagemClicavel.addEventListener('click', function() {
+    window.location.href = '/';
+});
+
+// function redirecionar() {
+//     window.open("https://api.whatsapp.com/message/QLVVFV3RGK33K1?autoload=1&app_absent=0", "_blank");
+// };
