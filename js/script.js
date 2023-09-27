523e132827ced2a8e65a5631a2786a96a5e812e2
BrunoDaPaz <nobrudapaz@gmail.com>
2023-09-27 00:19:51 -0300

    ajuste

--- js/script.js
+++ js/script.js
@@ -23,9 +23,5 @@ closeMenu.addEventListener('click', () => {
 });
 
 imagemClicavel.addEventListener('click', function() {
-    window.location.href = '/';
-});
-
-document.querySelector('.imagem-link').addEventListener('click', function() {
     window.location.href = 'https://api.whatsapp.com/message/QLVVFV3RGK33K1?autoload=1&app_absent=0';
 });
